bf2b3c770ac2a1fac592913ee75a4531a9f493da
Daniel Mathews <dannyl.mathews@gmail.com>
2015-04-27 07:40:16 -0700

    blank project1

--- WalkingBear/GameScene.swift
+++ WalkingBear/GameScene.swift
@@ -10,28 +10,8 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var bear : SKSpriteNode!
-    var bearWalkingFrames : [SKTexture]!
-    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        backgroundColor = (UIColor.blackColor())
-        
-        let bearAnimatedAtlas = SKTextureAtlas(named: "BearImages")
-        var walkFrames = [SKTexture]()
-        
-        let numImages = bearAnimatedAtlas.textureNames.count
-        for var i=1; i<=numImages/2; i++ {
-            let bearTextureName = "bear\(i)"
-            walkFrames.append(bearAnimatedAtlas.textureNamed(bearTextureName))
-        }
-        
-        bearWalkingFrames = walkFrames
-        
-        let firstFrame = bearWalkingFrames[0]
-        bear = SKSpriteNode(texture: firstFrame)
-        bear.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        addChild(bear)
         
     }
     
@@ -43,51 +23,14 @@ class GameScene: SKScene {
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         // Choose one of the touches to work with
         
-        let touch = touches.first as! UITouch
-        let location = touch.locationInNode(self)
-        var directionMultipler:CGFloat
-        
-        let bearVelocity = self.frame.size.width / 3.0
-        
-        let moveDifference = CGPointMake(location.x - bear.position.x, location.y - bear.position.y)
-        let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y + moveDifference.y)
-        
-        let moveDuration = distanceToMove / bearVelocity
-        
-        if (moveDifference.x < 0) {
-            directionMultipler = 1.0
-        } else {
-            directionMultipler = -1.0
-        }
-        
-        bear.xScale = fabs(bear.xScale) * directionMultipler
-        
-        if (bear.actionForKey("bearMoving") != nil){
-            bear.removeActionForKey("bearMoving")
-        }
-        
-        if (bear.actionForKey("walkingInPlaceBear") == nil){
-            walkingBear()
-        }
-        
-        let moveAction = (SKAction.moveTo(location, duration: Double(moveDuration)))
-        
-        let doneAction = SKAction.runBlock {
-            println("Animation Completed")
-            self.bearMoveEnded()
-        }
-
-        let moveActionWithDone = SKAction.sequence([moveAction, doneAction])
-        bear.runAction(moveActionWithDone, withKey: "bearMoving")
     }
     
     func walkingBear(){
-        bear.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(bearWalkingFrames, timePerFrame: 0.1, resize: false, restore: true)), withKey: "walkingInPlaceBear")
         
     }
     
     func bearMoveEnded(){
-        bear.removeAllActions()
+
     }
     
     override func update(currentTime: CFTimeInterval) {
